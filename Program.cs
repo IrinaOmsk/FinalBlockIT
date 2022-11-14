ec35258637911a5800ee04b5413c882f8dfe2ac9
Irina <I.M.ZUBKOVA@GMAIL.COM>
2022-11-15 00:15:21 +0600

    add Program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Задайте массив строк через пробел: ");
+
+
+string[] inputArray = Console.ReadLine().Split(' ');
+int N = inputArray.Length;
+string[] outputArray = new string[inputArray.Length];
+
+void CreatNewArray(string[] inputArray, string[] outputArray)
+{
+    int i = 0;
+    int j = 0;
+
+    while (i < N)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            outputArray[j] = inputArray[i];
+            j++;
+        }
+        i++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+CreatNewArray(inputArray, outputArray);
+PrintArray(outputArray);
